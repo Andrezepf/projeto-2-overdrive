f3fb20b6ffc1f35cb349211556247020d5cf0b20
Andrezepf <andrezepf@gmail.com>
2024-04-16 14:21:21 -0300

    Update Home.jsx

--- projeto-2-overdrive/src/pages/Home.jsx
+++ projeto-2-overdrive/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
               </div>
               <div className="card-body">
                 <p>Seja bem vindo à minha aplicação.</p>
-                <p>Esse projeto consiste em um CRUD para pessoas e empresas sem a necessidade de fazer login. Até o momento ele possui apenas o front-end, tornando a experiência um pouco limitada. </p>
+                <p>Esse projeto consiste no Front-end para um CRUD de pessoas e empresas sem a necessidade de fazer login. Neste estágio ele ainda não possui back-end então muitas das funções são apenas visuais. </p>
                 <p>Para começar a usá-lo basta usar os botões "Pessoas" e "Empresas" no topo da página. Espero que goste ;)</p>
               </div>
             </div>
